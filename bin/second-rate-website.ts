66c5a52caec851b003792399d007312ad673d84f
dev <dev@dev-machine.local>
2020-11-19 13:07:59 -0600

    chore: validate certificate

--- bin/second-rate-website.ts
+++ bin/second-rate-website.ts
@@ -3,5 +3,11 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { SecondRateWebsiteStack } from '../lib/second-rate-website-stack';
 
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION
+}
+
 const app = new cdk.App();
-new SecondRateWebsiteStack(app, 'SecondRateWebsiteStack');
+new SecondRateWebsiteStack(app, 'SecondRateWebsiteStack', { env })
+
--- lib/second-rate-website-stack.ts
+++ lib/second-rate-website-stack.ts
@@ -1,9 +1,32 @@
 import * as cdk from '@aws-cdk/core';
+import * as acm from '@aws-cdk/aws-certificatemanager';
+import * as route53 from '@aws-cdk/aws-route53';
 
 export class SecondRateWebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+      
+    const domain = new cdk.CfnParameter(this, 'DomainName', {
+      type: 'String',
+      description: 'Domain name',
+    })
 
-    // The code that defines your stack goes here
+    const zoneId = new cdk.CfnParameter(this, 'HostedZoneId', {
+      type: 'String',
+      description: 'Hosted Zone Id'
+    })
+
+    const domainName = domain.valueAsString
+    const hostedZoneId = zoneId.valueAsString
+
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+      hostedZoneId,
+      zoneName: domainName
+    })
+
+    new acm.DnsValidatedCertificate(this, 'AwsManagedCertificate', {
+      domainName,
+      hostedZone
+    })
   }
 }
