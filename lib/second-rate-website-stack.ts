33df684ccdc79048c0871f59d65cc3e52b7f8557
Darren <me@darrenholland.com>
2020-11-19 16:05:41 -0600

    chore: create infrastructure

--- lib/second-rate-website-stack.ts
+++ lib/second-rate-website-stack.ts
@@ -1,11 +1,23 @@
 import * as cdk from '@aws-cdk/core';
 import * as acm from '@aws-cdk/aws-certificatemanager';
 import * as route53 from '@aws-cdk/aws-route53';
+import { CloudFrontTarget } from '@aws-cdk/aws-route53-targets';
+
+import {
+  Bucket,
+  BlockPublicAccess,
+} from '@aws-cdk/aws-s3';
+
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+  ViewerCertificate,
+} from '@aws-cdk/aws-cloudfront';
 
 export class SecondRateWebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-      
+
     const domain = new cdk.CfnParameter(this, 'DomainName', {
       type: 'String',
       description: 'Domain name',
@@ -24,9 +36,42 @@ export class SecondRateWebsiteStack extends cdk.Stack {
       zoneName: domainName
     })
 
-    new acm.DnsValidatedCertificate(this, 'AwsManagedCertificate', {
+    const cert = new acm.DnsValidatedCertificate(this, 'AwsManagedCertificate', {
       domainName,
       hostedZone
     })
+
+    const bucket = new Bucket(this, 'StaticSiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity', {
+      comment: `OAI for ${domainName} website`
+    });
+
+    bucket.grantRead(originAccessIdentity);
+
+    const distribution = new CloudFrontWebDistribution(this, 'WebDistribution', {
+      viewerCertificate: ViewerCertificate.fromAcmCertificate(cert),
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity,
+          },
+          behaviors: [{isDefaultBehavior: true}]
+        },
+      ]
+    });
+
+    new route53.ARecord(this, 'WebAliasRecord', {
+      zone: hostedZone,
+      recordName: domainName,
+      target: route53.RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    });
   }
 }
