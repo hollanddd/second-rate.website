8a2ceb87468b73d6fb3167303680e80fcd201bbb
Darren <me@darrenholland.com>
2021-02-26 13:04:53 -0600

    feat: words motherfucker

--- website/src/App.js
+++ website/src/App.js
@@ -6,7 +6,31 @@ function App() {
     <div className="App">
       <header>
         <h1>This is a second rate website.</h1>
+        <p>
+          It's not as <a href="https://perfectmotherfuckingwebsite.com/">perfect</a> as <a href="http://bettermotherfuckingwebsite.com/">these</a> <a href="https://motherfuckingwebsite.com">motherfucking</a> <a href="https://thebestmotherfucking.website/">websites</a>.
+        </p>
       </header>
+      <h2>Seriously, it's more complicated than typograpy and green locks.</h2>
+      <p>That's how we get to the unordered list.</p>
+      <ul>
+        <li>Tests, motherfucker</li>
+        <li>Infrastructure as Code</li>
+        <li>Probably not an API but why the fuck not</li>
+        <li>Serverless for the same reason as above</li>
+      </ul>
+      <h2>This satire is a work in progress, you fuck.</h2>
+      <p>
+        <a href="https://github.com/hollanddd/second-reate.website">Improve this shit</a> or <a href="https://github.com/hollanddd/second-rate.website/blob/master/LICENSE">do what the f*ck you want to</a>.
+      </p>
+      <blockquote>
+        <p>"All right. Now, to start off, I want you to read these books."<br />-- Some Owl</p>
+      </blockquote>
+      <hr />
+      <h2 id="epilogue">Epilogue</h2>
+      <p>
+        Motivated by the effort put into <a href="https://motherfuckingwebsite.com/">motherfuckingwebsite.com</a>, <a href="http://bettermotherfuckingwebsite.com/">bettermotherfuckingwebsite.com</a> and <a href="https://duckduckgo.com/?q=motherfucking+website">others</a>.
+      </p>
+      <p></p>
       <Footer />
     </div>
   );
